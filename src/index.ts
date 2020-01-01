7e003b6f4e23aab7933e41afee11dd1102690e1c
Marcin <phisist@op,.pl>
2020-01-01 22:06:07 +0100

    Separating switches on same page part 2

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { createSeriesOfAttachables } from './attachable';
 
 createSeriesOfSwitches(4, '#turn-ts-on', '#turn-js-on', '.typescript', '.javascript');
 prepareSwitches('#turn-ts-on', '#turn-js-on', '.typescript', '.javascript');
+createSeriesOfSwitches(4, '#show-good', '#show-bad', '.good', '.bad');
 prepareSwitches('#show-good', '#show-bad', '.good', '.bad');
 
 createSeriesOfAttachables(3, '', 'flyout-error');
